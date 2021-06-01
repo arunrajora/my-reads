34ab0f951a98b7d2e1e3ede5ac3a207a2704668e
Arun Rajora <rajora.rajora2@gmail.com>
2021-06-01 10:37:00 +0530

    Updates

---
+++ precache-manifest.6d0c139f6ba6c4e7b3675223b57a06fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c628cc16989862414e4e17d215252fec",
+    "url": "/my-reads/index.html"
+  },
+  {
+    "revision": "4bb6bfbed5d2c37bc203",
+    "url": "/my-reads/static/css/main.db743235.chunk.css"
+  },
+  {
+    "revision": "387979440bbc6944d1b8",
+    "url": "/my-reads/static/js/2.51604405.chunk.js"
+  },
+  {
+    "revision": "4bb6bfbed5d2c37bc203",
+    "url": "/my-reads/static/js/main.83527aab.chunk.js"
+  },
+  {
+    "revision": "0d263240ce2f70cea861",
+    "url": "/my-reads/static/js/runtime~main.464cac55.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/my-reads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/my-reads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/my-reads/static/media/arrow-drop-down.78c6f53b.svg"
+  }
+]);
