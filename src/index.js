674487176298ff97efef8656e1f3913bece2a7fd
Arun Rajora <rajora.rajora2@gmail.com>
2021-06-01 10:37:58 +0530

    commit message

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
+console.log('found', process.env.PUBLIC_URL);
+
 ReactDOM.render(
   <BrowserRouter basename={`${process.env.PUBLIC_URL}/`}>
     <App />
